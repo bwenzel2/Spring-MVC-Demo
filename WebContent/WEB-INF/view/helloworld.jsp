caf9af3b80ffb0048b25abb2c8db8dc43a1cb8ad
Benjamin Wenzel <benwenzel@runtimeparadigm.com>
2018-07-03 19:47:28 -0400

    Initial commit

---
+++ WebContent/WEB-INF/view/helloworld.jsp
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	</head>
+	<body>
+		<h2>Spring MVC Demo - Home Page</h2>
+		<br><br>
+		Student name: ${param.studentName}
+	</body>
+</html>
---
+++ src/com/luv2code/springdemo/mvc/HomeController.java
@@ -0,0 +1,25 @@
+package com.luv2code.springdemo.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping("/")
+	public String showPage() {
+		//the Spring bean will append the prefix and the suffix from the bean def to this string 
+		//we're returning to get the full filepath and name (with the extension)
+		return "main-menu";
+	}
+	
+	@RequestMapping("/showForm")
+	public String showForm() {
+		return "helloworld-form";
+	}
+	
+	@RequestMapping("/processForm")
+	public String processForm() {
+		return "helloworld";
+	}
+}
